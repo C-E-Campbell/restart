f1a53a4bc2b3787a67420d4d03aa27ef5c2cd3a9
Max Hebert <maxhebert@Maxs-MacBook-Pro.local>
2019-12-17 14:30:32 -0700

    styling profile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,10 +1,11 @@
-/* eslint-disable array-callback-return */
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import BasicHeader from "../../Components/BasicHeader/BasicHeader";
 import "./Profile.style.scss";
 import Project from "../../Components/ProfileProject/ProfileProject";
 import axios from "axios";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import MainContent from "../../Components/MainContent/MainContent";
 
 class Profile extends Component {
   constructor(props) {
@@ -19,7 +20,15 @@ class Profile extends Component {
     const result = await axios.get(
       `/auth/get_link_campus_email/${this.props.user.id}`
     );
-    this.setState({ profileData: result.data[0] });
+    if (result.data[0] === true) {
+      this.setState({ profileData: result.data[0] });
+    } else {
+      const result = await axios.get(
+        `/auth/get_campus_email/${this.props.user.id}`
+      );
+      console.log(result.data);
+      this.setState({ profileData: result.data[0] });
+    }
   }
 
   render() {
@@ -30,11 +39,15 @@ class Profile extends Component {
     });
 
     return (
-      <React.Fragment>
+      <div className="whole-profile">
+        <Sidebar
+        loggedUser={this.props.user.id}
+        />
+        <MainContent>
         <div className="profile-main">
-          <BasicHeader />
+          
 
-          <div className="profile-pic"></div>
+          
 
           <div className="profile-info">
             <div className="profile-details">
@@ -42,21 +55,27 @@ class Profile extends Component {
               {this.props.user.last}
             </div>
 
-            <div>
+            {/* <div> */}
               <div>Campus: {this.state.profileData.campus}</div>
               <div>Email: {this.state.profileData.email}</div>
-              <a target="target_blank" href={this.state.profileData.linkedin}>
-                <i className="fab fa-linkedin"></i>
-              </a>
-            </div>
+              {!this.state.profileData.linkedin ? (
+                <></>
+              ) : (
+                <a target="target_blank" href={this.state.profileData.linkedin}>
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              )}
+            {/* </div> */}
           </div>
-
           <h4>My Projects</h4>
+          <div className="profile-grid-container">
           <div className="profile-container">
             <div className="profile-grid"> {mappedProjects}</div>
+            </div>
           </div>
         </div>
-      </React.Fragment>
+        </MainContent>
+      </div>
     );
   }
 }
