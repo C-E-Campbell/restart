7021223c4de66d19f0f07e484eaeac7cacbcf660
Charles <charles.e.campbell1989@gmail.com>
2020-06-06 02:13:58 -0400

    need to figure out db stuff

--- server/auth_controller.js
+++ server/auth_controller.js
@@ -1,12 +1,12 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
 module.exports = {
   register: async (req, res, next) => {
     const { first, last, email, password, campus, status } = req.body;
-    const db = req.app.get("db");
+    const db = req.app.get('db');
     const foundUser = await db.find_user_by_email(email);
     if (foundUser.length) {
-      res.status(400).send("user already exist");
+      res.status(400).send('user already exist');
     } else {
       const saltRounds = 12;
       const salt = await bcrypt.genSalt(saltRounds);
@@ -17,14 +17,14 @@ module.exports = {
         email,
         hashedPassword,
         campus,
-        status
+        status,
       ]);
 
       req.session.user = req.session.user = {
         id: newUser[0].user_id,
         email: newUser[0].email,
         first: newUser[0].first,
-        last: newUser[0].last
+        last: newUser[0].last,
       };
 
       res.status(200).send(req.session.user);
@@ -32,14 +32,14 @@ module.exports = {
   },
   login: async (req, res, next) => {
     const { email, password } = req.body;
-    const db = req.app.get("db");
+    const db = req.app.get('db');
 
     try {
       const checkForUser = await db.find_user_by_email([email]);
       if (!checkForUser[0]) {
         res
           .status(401)
-          .send("Cant find that account. You may need to register");
+          .send('Cant find that account. You may need to register');
       } else {
         client.set(`${email}:password`, password); // store session instead
         let checkPass = bcrypt.compareSync(password, checkForUser[0].password);
@@ -48,11 +48,11 @@ module.exports = {
             id: checkForUser[0].user_id,
             email: checkForUser[0].email,
             first: checkForUser[0].first,
-            last: checkForUser[0].last
+            last: checkForUser[0].last,
           };
           res.status(200).send(req.session.user);
         } else {
-          res.status(401).send("not valid email/pass");
+          res.status(401).send('not valid email/pass');
         }
       }
     } catch (error) {
@@ -61,17 +61,22 @@ module.exports = {
   },
   logout: (req, res, next) => {
     req.session.destroy();
-    res.status(200).send("Logout successful");
+    res.status(200).send('Logout successful');
   },
   getNames: async (req, res, next) => {
-    const db = req.app.get("db");
-    const result = await db.get_names_by_id();
-    return res.status(200).send(result);
+    const db = req.app.get('db');
+
+    try {
+      const result = await db.get_names_by_id();
+      return res.status(200).send(result);
+    } catch (err) {
+      console.log(err);
+    }
   },
   getCampusAndEmail: async (req, res, next) => {
-    const db = req.app.get("db");
+    const db = req.app.get('db');
     const { id } = req.params;
     const result = await db.get_campus_email([+id]);
     return res.status(200).send(result);
-  }
+  },
 };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const session = require("express-session");
-const massive = require("massive");
+const session = require('express-session');
+const massive = require('massive');
 // const socketio = require("socket.io");
-const profileCTRL = require("./profile_controller");
-const fileUpload = require("express-fileupload");
+const profileCTRL = require('./profile_controller');
+const fileUpload = require('express-fileupload');
 
 app.use(express.static(`${__dirname}/../build`));
 
@@ -18,8 +18,8 @@ const {
   login,
   checkCache,
   getNames,
-  getCampusAndEmail
-} = require("./auth_controller");
+  getCampusAndEmail,
+} = require('./auth_controller');
 
 const {
   userProjects,
@@ -30,8 +30,8 @@ const {
   addFeedback,
   editFeedback,
   deleteFeedback,
-  getAllFeedback
-} = require("./projects_controller");
+  getAllFeedback,
+} = require('./projects_controller');
 
 const {
   getAllIdeas,
@@ -41,77 +41,72 @@ const {
   addIdeaFeedback,
   editIdeaFeedback,
   deleteIdeaFeedback,
-  getIdeaFeedback
-} = require("./idea_controller");
+  getIdeaFeedback,
+} = require('./idea_controller');
 
-const { getCampusInfo, getCampusLinkEamil } = require("./chart_controller");
+const { getCampusInfo, getCampusLinkEamil } = require('./chart_controller');
 
 const { CONNECTION_STRING, SESSION_SECRET, SERVER_PORT } = process.env;
 app.use(
   session({
-    secret: "sertertert",
+    secret: 'sertertert',
     resave: false,
     saveUninitialized: false,
     cookies: {
-      maxAge: 1000 * 60 * 60 * 24 * 14
-    }
+      maxAge: 1000 * 60 * 60 * 24 * 14,
+    },
   })
 );
 
-massive(
-  "postgres://gmxzovjfjktxng:97a3ee3e95fe129285ea95c470854c3ac5a762df20cce6c8de478af6969f3a78@ec2-50-19-95-77.compute-1.amazonaws.com:5432/dft043im6lq5dn?ssl=true"
-).then(db => {
-  console.log("database connected");
-  app.set("db", db);
-});
+massive({
+  connectionString:
+    'postgres://gmxzovjfjktxng:97a3ee3e95fe129285ea95c470854c3ac5a762df20cce6c8de478af6969f3a78@ec2-50-19-95-77.compute-1.amazonaws.com:5432/dft043im6lq5dn?ssl=true',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+  .then((dbInstance) => {
+    app.set('db', dbInstance);
+  })
+  .catch((err) => console.log(err));
 
-app.post("/auth/register", register);
-app.post("/auth/login", login);
+app.post('/auth/register', register);
+app.post('/auth/login', login);
 
-app.post("/auth/imageupload/:id", profileCTRL.imgUpload);
-app.delete("/auth/logout", logout);
-app.get("/auth/getAllProjects", getAllProjects);
-app.get("/auth/getProfilePhoto/:id", profileCTRL.getProfilePhoto);
-app.get("/auth/userProjects/:user_id", userProjects);
-app.get("/auth/getNames", getNames);
-app.delete("/auth/delete_project/:project_id", deleteProject);
-app.put("/auth/edit_project", editproject);
-app.post("/auth/addProject", addProject);
+app.post('/auth/imageupload/:id', profileCTRL.imgUpload);
+app.delete('/auth/logout', logout);
+app.get('/auth/getAllProjects', getAllProjects);
+app.get('/auth/getProfilePhoto/:id', profileCTRL.getProfilePhoto);
+app.get('/auth/userProjects/:user_id', userProjects);
+app.get('/auth/getNames', getNames);
+app.delete('/auth/delete_project/:project_id', deleteProject);
+app.put('/auth/edit_project', editproject);
+app.post('/auth/addProject', addProject);
 
-app.get("/auth/get_all_feedback/:id", getAllFeedback);
-app.post("/auth/add_feedback", addFeedback);
-app.put("/auth/edit_feedback", editFeedback);
-app.delete("/auth/delete_feedback/:feedback_id/:project_id", deleteFeedback);
+app.get('/auth/get_all_feedback/:id', getAllFeedback);
+app.post('/auth/add_feedback', addFeedback);
+app.put('/auth/edit_feedback', editFeedback);
+app.delete('/auth/delete_feedback/:feedback_id/:project_id', deleteFeedback);
 
-app.get("/auth/get_ideas", getAllIdeas);
-app.post("/auth/add_idea", addIdea);
-app.put("/auth/edit_idea", editIdea);
-app.delete("/auth/delete_idea/:idea_id", deleteIdea);
+app.get('/auth/get_ideas', getAllIdeas);
+app.post('/auth/add_idea', addIdea);
+app.put('/auth/edit_idea', editIdea);
+app.delete('/auth/delete_idea/:idea_id', deleteIdea);
 
-app.get("/auth/get_idea_feedback", getIdeaFeedback);
-app.post("/auth/add_idea_feedback", addIdeaFeedback);
-app.put("/auth/edit_idea_feedback", editIdeaFeedback);
-app.delete("/auth/delete_idea_feedback/:idea_feedback_id", deleteIdeaFeedback);
+app.get('/auth/get_idea_feedback', getIdeaFeedback);
+app.post('/auth/add_idea_feedback', addIdeaFeedback);
+app.put('/auth/edit_idea_feedback', editIdeaFeedback);
+app.delete('/auth/delete_idea_feedback/:idea_feedback_id', deleteIdeaFeedback);
 
-app.get("/auth/get_campus", getCampusInfo);
-app.get("/auth/get_campus_email/:id", getCampusAndEmail);
-app.get("/auth/get_link_campus_email/:id", getCampusLinkEamil);
+app.get('/auth/get_campus', getCampusInfo);
+app.get('/auth/get_campus_email/:id', getCampusAndEmail);
+app.get('/auth/get_link_campus_email/:id', getCampusLinkEamil);
 
 let port = SERVER_PORT || 4001;
-const expressServer = app.listen(port, () =>
-  console.log(`up and running on port ${port}`)
-);
+app.listen(port, () => console.log(`up and running on port ${port}`));
 
-// const io = socketio(expressServer);
+const path = require('path');
 
-// io.on("connection", socket => {
-//   socket.on("message", message => {
-//     io.emit("newMessage", message);
-//   });
-// });
-
-const path = require("path");
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
