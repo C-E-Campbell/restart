93b4e81382d4e9b534deacfb66b57b8df3031e9c
Charles <charles.e.campbell1989@gmail.com>
2020-07-05 17:48:42 -0400

    updates

--- server/profile_controller.js
+++ server/profile_controller.js
@@ -1,13 +1,13 @@
 module.exports = {
   imgUpload: (req, res, next) => {
-    const db = req.app.get("db");
+    const db = req.app.get('db');
     const { id } = req.params;
     if (req.files === null) {
-      return res.status(400).json({ msg: "No File Uploaded" });
+      return res.status(400).json({ msg: 'No File Uploaded' });
     }
 
     const file = req.files.file;
-    file.mv(__dirname + "/../build/uploads/" + id + file.name, err => {
+    file.mv(__dirname + '/../build/uploads/' + id + file.name, (err) => {
       if (err) {
         console.log(err);
         return res.status(500).send(err);
@@ -15,19 +15,18 @@ module.exports = {
       db.insert_profile_photo([`./uploads/${id}${file.name}`, id]);
       res.json({
         fileName: file.name,
-        filePath: `./uploads/${id}${file.name}`
+        filePath: `./uploads/${id}${file.name}`,
       });
     });
   },
   getProfilePhoto: async (req, res) => {
-    const db = req.app.get("db");
+    const db = req.app.get('db');
     const { id } = req.params;
     try {
       const result = await db.get_profile_photo([id]);
-      console.log(result);
       res.status(200).send(result[0].profile_image);
     } catch (err) {
       console.log(err);
     }
-  }
+  },
 };
